528a5cfaf812d3ca1fe23da9505dc1b03379d28a
ethandashwood <72386500+ethandashwood@users.noreply.github.com>
2020-11-11 19:33:41 +0000

    stove and background

--- Assets/scripts/playermove.cs
+++ Assets/scripts/playermove.cs
@@ -8,14 +8,13 @@ public class playermove : MonoBehaviour
     public Animator anim;
     public GameObject Camera;
     Rigidbody2D rb;
-    // Start is called before the first frame update
+
     void Start()
     {
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
 
